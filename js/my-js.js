bce5bab57e20bb360470f364e7ac571d30a97c9a
highdance <mxe.hardy@gmail.com>
2015-09-25 16:30:07 +0200

    sexo & seance

--- js/my-js.js
+++ js/my-js.js
@@ -1,21 +1,26 @@
 $(document).ready(function() {
-    /* ScrollTop on anchor */
-    $('a.l-scroll').on('click', function(evt) {
-        evt.preventDefault();
-        var target = $(this).attr('href');
-        if (target == '#l-top')
-            $('html, body')
-            .stop()
-            .animate({
-                scrollTop: $(target).offset().top - 1000
-            }, 1000);
-        else
-            $('html, body')
-            .stop()
-            .animate({
-                scrollTop: $(target).offset().top - 35
-            }, 1000);
-    });
+  $('.collapsible').collapsible({
+    accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+  });
+  /* ScrollTop on anchor */
+  $('a.l-scroll').on('click', function(evt) {
+    evt.preventDefault();
+    var target = $(this).attr('href');
+    if (target == '#l-top')
+      $('html, body')
+      .stop()
+      .animate({
+        scrollTop: $(target).offset().top - 1000
+      }, 1000);
+    else
+      $('html, body')
+      .stop()
+      .animate({
+        scrollTop: $(target).offset().top - 35
+      }, 1000);
+  });
 
-    $('.tooltipped').tooltip({delay: 50});
+  $('.tooltipped').tooltip({
+    delay: 50
+  });
 });
